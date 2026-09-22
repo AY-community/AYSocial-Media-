5468f75bff4459ea60d17302e5152666e7c89219
Aymen chedri <4ymene@gmail.com>
2026-09-23 01:48:54 +0200

    update header display reels

--- Client/src/Layouts/MainLayouts/Header.jsx
+++ Client/src/Layouts/MainLayouts/Header.jsx
@@ -80,7 +80,9 @@ function Header({ className, hideOnMobile = false }) {
         return;
       }
 
-      if (location.pathname === "/" || location.pathname.startsWith("/explore")) {
+      if (location.pathname === "/reel") {
+        setIsHeaderHidden(false);
+      } else if (location.pathname === "/" || location.pathname.startsWith("/explore")) {
         const shouldHide = scrollDelta > 0 && currentScrollY > 70;
         setIsHeaderHidden(shouldHide);
       } else if (location.pathname.startsWith("/messages")) {
